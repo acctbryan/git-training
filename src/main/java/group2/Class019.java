021e066ff41f75a00e0eade613760495fde22f3e
JiangWeiLing <z200571893@gmail.com>
2022-11-21 16:34:01 +0800

    add getter/setter

--- src/main/java/group2/Class019.java
+++ src/main/java/group2/Class019.java
@@ -3,4 +3,17 @@ package group2;
 public class Class019 {
 	private int x123;
 	private String zzz123;
+	public int getX123() {
+		return x123;
+	}
+	public void setX123(int x123) {
+		this.x123 = x123;
+	}
+	public String getZzz123() {
+		return zzz123;
+	}
+	public void setZzz123(String zzz123) {
+		this.zzz123 = zzz123;
+	}
+	
 }
