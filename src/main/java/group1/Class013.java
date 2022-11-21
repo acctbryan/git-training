efbc550f25f06333080a0028fc05f1db26b0ea38
Zack <zack@test.com>
2022-11-21 16:34:14 +0800

    test1

--- src/main/java/group1/Class013.java
+++ src/main/java/group1/Class013.java
@@ -1,12 +1,18 @@
 package group1;
 
 public class Class013 {
- void test(int c,String name) {
-	 
-	 System.out.println(c+""+name);
-	 int x = 1+111;
-	 
-System.out.println(x);
 
-}
+	private void test(int c,String name) {
+		 System.out.println(c);
+		 System.out.println(name);
+		 int x = 1+111;
+		 
+		System.out.println(x);
+	}
+
+	private static void test2(int x) {
+		System.out.println(x);
+
+	}
+
 }
