d4ca99dcc44403b096ea0c7e8e96a8187d3a1e86
胡竣瑋 <willyhu@wistronits.com>
2022-11-21 16:53:00 +0800

    modify

--- src/main/java/group1/Class013.java
+++ src/main/java/group1/Class013.java
@@ -1,30 +1,15 @@
 package group1;
 
 public class Class013 {
-<<<<<<< Upstream, based on branch 'master' of https://github.com/acctbryan/git-training.git
-=======
- void test(int c,String name) {
+
+ public void test (int c,String name) {
 	 for (int i = 100 ; i <= 50 ; i--) {
 		 System.out.print(i);
-		 System.out.print(c);
-		 System.out.print(c);
 	 }
-	 System.out.println(c+""+name);
+	 System.out.println(c +""+name);
 	 int x = 1+111;
 	 
-System.out.println(x);
->>>>>>> b7cef17 modify class013
-
-	private void test(int c,String name) {
-		 System.out.println(c);
-		 System.out.println(name);
-		 int x = 1+111;
-		 
-		System.out.println(x);
-	}
-
-	private static void test2(int x) {
-		System.out.println(x);
+	 System.out.println(x);
 
 	}
 
