134a431c1a5fe0f6de021ebe8f9747bc7f398079
Wistronits <estherliu@wistronits.com>
2022-11-23 14:11:11 +0800

    add toString

--- src/main/java/test/Function036.java
+++ src/main/java/test/Function036.java
@@ -4,4 +4,12 @@ public class Function036 {
 
 	private String test;
 	private String test2;
+	
+	@Override
+	public String toString() {
+		return "Function036 [test=" + test + ", test2=" + test2 + "]";
+	}
+	
+	
+	
 }
